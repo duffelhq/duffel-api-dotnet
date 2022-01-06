6a40da3e8d72b4354213eb7d6c08369ccd7095e0
Piotr Witkowski <piotr.witkowski@outlook.com>
2022-01-06 18:09:04 +0000

    Refactor to be compatible with .Net 5.0 (to enable git actions)

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,24 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Duffel.ApiClient;
 using Duffel.ApiClient.Interfaces.Models;
 using Duffel.ApiClient.Interfaces.Models.Requests;
 
-var client = new DuffelApiClient(args[0]);
-var offers = await client.CreateOffersRequest(new OffersRequest
+namespace Examples
 {
-    Passengers = new List<Passenger> { new Passenger { PassengerType = PassengerType.Adult }},
-    RequestedSources = new List<string> { "duffel_airways" },
-    Slices = new List<Slice>
+    public class Examples
     {
-        new Slice
-        {
-            Origin = "SFO",
-            Destination = "JFK",
-            DepartureDate = DateTime.Now.AddDays(30).ToString("yyyy-MM-dd")
-        }
-    }
-});
 
-offers.Slices.ToList().ForEach(slice => Console.WriteLine($"Slice: {slice.Origin.Id} -> {slice.Destination.Id}"));
+        public static async Task Main(string[] args)
+        {
+            var client = new DuffelApiClient(args[0]);
+            var offers = await client.CreateOffersRequest(new OffersRequest
+            {
+                Passengers = new List<Passenger> { new Passenger { PassengerType = PassengerType.Adult } },
+                RequestedSources = new List<string> { "duffel_airways" },
+                Slices = new List<Slice>
+                {
+                    new Slice
+                    {
+                        Origin = "SFO",
+                        Destination = "JFK",
+                        DepartureDate = DateTime.Now.AddDays(30).ToString("yyyy-MM-dd")
+                    }
+                }
+            });
+
+            offers.Slices.ToList()
+                .ForEach(slice => Console.WriteLine($"Slice: {slice.Origin.Id} -> {slice.Destination.Id}"));
+        }
+
+    }
+    
+}
+
