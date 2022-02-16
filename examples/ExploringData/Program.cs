9045358146cfc88f8adfc3d376c00e42dbefa713
Piotr Witkowski <piotr.witkowski@outlook.com>
2022-02-16 10:30:35 +0000

    Removed unrelated request for flight offers

--- examples/ExploringData/Program.cs
+++ examples/ExploringData/Program.cs
@@ -33,24 +33,3 @@ Console.WriteLine($"Retrieved {airlines.Count()} airlines");
 Console.WriteLine($"Loading single airline {airlines.First().AirlineName}");
 var airline = await client.Airlines.Get(airlines.First().Id);
 Console.WriteLine($"Airline IATA: {airline.IataCode}");
-
-var offersRequest = new OffersRequest
-{
-    CabinClass = CabinClass.Economy,
-    Passengers = new List<Passenger>() { new Passenger { PassengerType = PassengerType.Adult } },
-    //RequestedSources = new List<string> { "lufthansa_group" },
-    Slices = new List<Slice>
-    {
-        new()
-        {
-            // We use a nonsensical route to make sure we get speedy, reliable "Duffel Airways" results
-            Origin = "LHR",
-            Destination = "STN",
-            DepartureDate = DateTime.Now.AddMonths(12).ToString("yyyy-MM-dd")
-        }
-    }
-};
-
-Console.WriteLine("Requesting offers");
-var response = await client.OfferRequests.Create(offersRequest, returnOffers: false);
-Console.WriteLine($"Retrieved {response.Offers.Count()} offers");
