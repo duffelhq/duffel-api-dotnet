4b47bd39717102fac8a4d08ff0a68e109449f31b
Maarten Balliauw <maarten@maartenballiauw.be>
2022-10-20 21:25:08 +0200

    Add Airline LogoLockupUrl and LogoSymbolUrl properties

--- Duffel.ApiClient/Models/Airline.cs
+++ Duffel.ApiClient/Models/Airline.cs
@@ -9,17 +9,30 @@ namespace Duffel.ApiClient.Models
         /// </summary>
         [JsonProperty("name")]
         public string AirlineName { get; set; }
-        
+
         /// <summary>
         /// Duffel's unique identifier for the airline
         /// </summary>
         [JsonProperty("id")]
         public string Id { get; set; }
-        
+
         /// <summary>
         /// The two-character IATA code for the airline. This may be null for non-IATA carriers.
         /// </summary>
         [JsonProperty("iata_code")]
         public string IataCode { get; set; }
+
+        /// <summary>
+        /// Path to a svg of the airline lockup logo. A lockup logo is also called a combination logo, in which it combines the logotype and logomark.
+        /// This may be null if no logo is available.
+        /// </summary>
+        [JsonProperty("logo_lockup_url")]
+        public string LogoLockupUrl { get; set; }
+
+        /// <summary>
+        /// Path to a svg of the airline logo. This may be null if no logo is available.
+        /// </summary>
+        [JsonProperty("logo_symbol_url")]
+        public string LogoSymbolUrl { get; set; }
     }
 }
